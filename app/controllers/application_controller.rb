f40f9c2c42ad0c7e447be19fe622c10dd2850047
Morgan Fudge <mln_mf@hotmail.com>
2018-02-07 02:08:57 -0330

    Added list CRUD routes with pages, Fixed issue with helper throwing error if session didn't exist, Fixed issue with list login check regex

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,6 +10,12 @@ class ApplicationController < Sinatra::Base
         set :views, 'app/views'
     end
 
+    helpers do
+        def current_user
+            User.find(session[:user_id]) if session[:user_id]
+        end
+    end
+
     before %r{/(signup|login)} do
         #Send user to list if they are already logged in
         redirect to '/list' if session[:user_id]
@@ -35,7 +41,7 @@ class ApplicationController < Sinatra::Base
         redirect to '/signup' if params[:username] == "" && params[:email] == "" && params[:password] == ""
 
         #Create new user
-        user = Users.new(username: params[:username], email: params[:email], password: params[:password])
+        user = User.new(username: params[:username], email: params[:email], password: params[:password])
 
         #Try to save user and login
         if user.save
@@ -58,7 +64,7 @@ class ApplicationController < Sinatra::Base
         redirect to '/login' if params[:email] == "" && params[:password] == ""
 
         #Authenticate user
-        user = Users.find_by(email: params[:email]).try(:authenticate, params[:password])
+        user = User.find_by(email: params[:email]).try(:authenticate, params[:password])
 
         #Check user auth
         if user
@@ -70,8 +76,14 @@ class ApplicationController < Sinatra::Base
         end
     end
 
+    #Logout
+    get '/logout' do
+        session[:user_id] = nil
+        redirect to '/'
+    end
+
     #Run before any list routes
-    before '/list/*' do
+    before /\/list\/(.*)|\/list/ do
         #Send user to index if they are not logged in
         redirect to '/' unless session[:user_id]
     end
@@ -79,14 +91,56 @@ class ApplicationController < Sinatra::Base
     #Lists
     get '/list' do
         #Display user's list
-        @user = Users.find_by(id: session[:user_id])
+        @user = current_user
         erb :'list/list'
     end
 
-    get '/logout' do
-        session[:user_id] = nil
+    #Add task
+    get'/list/add' do
+        erb :'list/add'
+    end
 
-        redirect to '/'
+    post '/list/add' do
+        task = Task.create(content: params[:content])
+        current_user.tasks << task
+        redirect to '/list/' + task.id.to_s
+    end
+
+    #Edit task
+    get '/list/:id/edit' do |id|
+        #Find task
+        @task = Task.find(id)
+        #Render edit
+        erb :'list/edit'
+    end
+
+    patch '/list/:id' do |id|
+        #Find task
+        task = Task.find(id)
+        #Update task if it belongs to the current user
+        task.update(content: params[:content]) if task.user_id == current_user.id
+        #Redirect to task
+        redirect to '/list/' + id.to_s
+    end
+
+    #Delete task
+    delete '/list/:id' do |id|
+        #Find task
+        task = Task.find(id)
+        #Delete task if it belongs to the current user
+        task.delete if task.user_id == current_user.id
+        #Redirect to list
+        redirect to '/list'
+    end
+
+    #Task detailed
+    get '/list/:id' do |id|
+        #Find task
+        @task = Task.find(id)
+        #Check if task belongs to user
+        redirect to '/list' unless @task.user_id == current_user.id
+        #Render task
+        erb :'list/show'
     end
 
 end
